4fa72a14dcbbf1799794bcd8daee938c1b343c6f
saiprasad33 <46974357+saiprasad33@users.noreply.github.com>
2019-08-10 14:30:17 +0530

    Add files via upload

---
+++ denso.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+    int x,p;
+
+    scanf("%d %d",&x,&p);
+
+    int i=x;
+
+    while(i>0)
+
+    {
+
+       
+         if(i<=0)
+
+		{
+
+		        break;
+        
+        }
+
+ 	 	else
+
+	        {
+
+		        printf("%d \t",i);
+
+	        }
+
+	        i=(int)((i)*(100-p)/100);
+	
+    }
+
+    if(i<=0)
+
+    {
+
+        printf("FREE");
+    
+}
+
+}
